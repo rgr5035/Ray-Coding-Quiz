f2a91df0c39cb436f022924f20edcf7340404e2a
Robby Ray <rgr5035@gmail.com>
2020-10-14 17:20:44 -0600

    added code to JS file to improve functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //VARIABLE DECLARATION
 const startButton = document.getElementById('start-btn');
 const questionContainerEl = document.getElementById('questions-container');
+const questionEl = document.getElementById("question");
+const answerButtonsEl = document.getElementById("answer-buttons");
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
 
@@ -17,12 +19,17 @@ function startGame () {
     console.log('started');
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContainerEl.classList.remove('hide');
     setNextQuestion()
 }
 
 function setNextQuestion (){
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
+function showQuestion(question) {
+    questionEl.innerText = question.question
 }
 
 function selectAnswer () {
@@ -40,44 +47,44 @@ const questions = [
         ]
     },
     {
-        question: "What is the correct HTML element for inserting a line break?\n(a) <break>\n(b) <br>\n(c) <lb>\n(d) <lbreak>",
+        question: "What is the correct HTML element for inserting a line break?",
         answers: [
             {text: "<br>", correct: true},
             {text: "<break>", correct: false},
             {text: "<lbreak>", correct: false},
             {text: "<lb>", correct: false},
         ]
-    },
-    {
-        question: "What does CSS stand for?\n(a) Colorful Style Sheets\n(b) Creative Style Sheets\n(c) Computer Style Sheets\n(d) Cascading Style Sheets",
-        answer: "d"
-    },
-    {
-        prompt: "Which of the following is the correct syntax to display 'Coding is fun!' in an alert box using JavaScript?\n(a) confirm('Coding is Fun!')\n(b) msg('Coding is Fun!)\n(c) prompt('Coding is Fun!')\n(d) alert('Coding is Fun!)",
-        answer: "d"  
-    },
-    {
-        prompt: "What is the correct syntax for referring to an external script called 'geek.js'?\n(a) <script src='geek.js'>\n(b) <script href='geek.js'\n(c) <script ref='geek.js'>\n(d) <script name='geek.js'>",
-        answer: "a"
-    },
-    {
-        prompt: "What does HTML stand for?\n(a) Hyperlinks and Text Markup Language\n(b) Home Tool Markup Language\n(c) Hyper Text Markup Language",
-        answer: "c"
-    },
-    {
-        prompt: "Where in an HTML document is the correct place to refer to an external style sheet?\n(a) The <head> section\n(b) At the end of the document\n(c) The <body> section",
-        answer: "a"
-    },
-    {
-        prompt: "The external JavaScript file must contain <script> tag. True or False?\n(a) True\n(b) False",
-        answer: "b"
-    },
-    {
-        prompt: "Which is the correct CSS syntax?\n(a) {body;color;black;}\n(b) body:color=black\n(c) body {color;black}\n(d) {body:color=black}",
-        answer: "c"
-    },
-    {
-        prompt: "How can you make a bulleted list?\n(a) <dl>\n(b) <list>\n(c) <ol>\n(d) <ul>",
-        answer: "d"
-    },
+    }
+    // {
+    //     question: "What does CSS stand for?\n(a) Colorful Style Sheets\n(b) Creative Style Sheets\n(c) Computer Style Sheets\n(d) Cascading Style Sheets",
+    //     answer: "d"
+    // },
+    // {
+    //     prompt: "Which of the following is the correct syntax to display 'Coding is fun!' in an alert box using JavaScript?\n(a) confirm('Coding is Fun!')\n(b) msg('Coding is Fun!)\n(c) prompt('Coding is Fun!')\n(d) alert('Coding is Fun!)",
+    //     answer: "d"  
+    // },
+    // {
+    //     prompt: "What is the correct syntax for referring to an external script called 'geek.js'?\n(a) <script src='geek.js'>\n(b) <script href='geek.js'\n(c) <script ref='geek.js'>\n(d) <script name='geek.js'>",
+    //     answer: "a"
+    // },
+    // {
+    //     prompt: "What does HTML stand for?\n(a) Hyperlinks and Text Markup Language\n(b) Home Tool Markup Language\n(c) Hyper Text Markup Language",
+    //     answer: "c"
+    // },
+    // {
+    //     prompt: "Where in an HTML document is the correct place to refer to an external style sheet?\n(a) The <head> section\n(b) At the end of the document\n(c) The <body> section",
+    //     answer: "a"
+    // },
+    // {
+    //     prompt: "The external JavaScript file must contain <script> tag. True or False?\n(a) True\n(b) False",
+    //     answer: "b"
+    // },
+    // {
+    //     prompt: "Which is the correct CSS syntax?\n(a) {body;color;black;}\n(b) body:color=black\n(c) body {color;black}\n(d) {body:color=black}",
+    //     answer: "c"
+    // },
+    // {
+    //     prompt: "How can you make a bulleted list?\n(a) <dl>\n(b) <list>\n(c) <ol>\n(d) <ul>",
+    //     answer: "d"
+    // },
 ]
